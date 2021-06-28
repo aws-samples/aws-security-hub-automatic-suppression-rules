1af95bb0b39c9f8b00d53d8b6c39e0c75cc9bf98
jjjoy@amazon.com <jjjoy@amazon.com>
2021-06-28 14:45:31 -0400

    add generate_bootstrap_version_rule so can generate cfn

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ from sechub_batch_update.sechub_batch_update_stack import SechubBatchUpdateStack
 from sechub_batch_update.vpc_test import VPCTest
 
 app = core.App()
-SechubBatchUpdateStack(app, "sechub-finding-suppression")
+SechubBatchUpdateStack(
+    app,
+    "sechub-finding-suppression",
+    synthesizer=core.DefaultStackSynthesizer(
+        generate_bootstrap_version_rule=False,
+    ),
+)
 VPCTest(app, "vpc-test-suppression")
 
 app.synth()
